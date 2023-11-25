98fe5ac2f416800467ebf41d478fd009b1dd13a3
AaronCronnelly <G00403528@gmit.ie>
2023-11-25 17:46:54 +0000

    working on making different methods for the server

--- Sever/BankingServer/src/server/ServerThread.java
+++ Sever/BankingServer/src/server/ServerThread.java
@@ -1,69 +1,82 @@
 package server;
+
 import java.net.Socket;
 import java.io.*;
 
-
-public class ServerThread extends Thread{
+public class ServerThread extends Thread {
 
 	Socket myConnection;
 	ObjectOutputStream out;
 	ObjectInputStream in;
 	String message;
-	
-	public ServerThread(Socket s)
-	{
+
+	public ServerThread(Socket s) {
 		myConnection = s;
 	}
-	
-	public void run()
-	{
-		try
-		{
+
+	public void run() {
+		try {
 			out = new ObjectOutputStream(myConnection.getOutputStream());
 			out.flush();
 			in = new ObjectInputStream(myConnection.getInputStream());
-		
-			//Server Comms
-			
-			sendMessage("Please enter 1 to ADD or 2 to SUBTRACT");
-			message = (String)in.readObject();
-		
-			if(message.equalsIgnoreCase("1"))
-			{
-				
+
+			// Server Comms
+
+			sendMessage("Welcome, SignIn(1), Register(2)");
+			message = (String) in.readObject();
+
+			if (message.equalsIgnoreCase("1")) {
+				/*
+				 * If the user choose sign in then they are going to be asked to
+				 */
+
 			}
-			
-			else if(message.equalsIgnoreCase("2"))
-			{
-				
+
+			else if (message.equalsIgnoreCase("2")) {// When the user registers they will be brought straigt to the menu
+
 			}
-			
-			
+
 			in.close();
 			out.close();
+		} catch (ClassNotFoundException classnot) {
+			System.err.println("Data received in unknown format");
+		} catch (IOException e) {
+
 		}
-		catch(ClassNotFoundException classnot)
-		{
-					System.err.println("Data received in unknown format");
-		}
-		catch(IOException e)
-		{
-			
-		}
-		
-		
+
 	}
-	
-	void sendMessage(String msg)
-	{
-		try{
+
+	void sendMessage(String msg) {
+		try {
 			out.writeObject(msg);
 			out.flush();
 			System.out.println("server>" + msg);
-		}
-		catch(IOException ioException){
+		} catch (IOException ioException) {
 			ioException.printStackTrace();
 		}
 	}
+
+	/****** METHODS ******/
+	/* Authentachation */
+//unsure if this method will be used yet have to wait and see
 	
+	/* Menu */
+//This is going to give the auth user the ability to acces other mehtods
+	
+	
+	/* Lodgment */
+//Allows the user to lodge money into the account
+	
+	/* Retrieve all registered users */
+//Allow the suer to see all registered users
+	
+	/* Transfer money */
+//Allow user to transfare money 
+	
+	/* View transaction history */
+//Allow user to view transaction history
+	
+	/* Update Password */
+//Allow user to update passowrd
 }
+
